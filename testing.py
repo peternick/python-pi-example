498d31dde4cdb53148c9cf82ce2bd6a83c20454e
peter <peternick2456@gmail.com>
2019-06-02 11:15:33 -0400

    adding testing.py to testU

---
+++ testing.py
@@ -0,0 +1,22 @@
+
+myList= [1,2,3,4]
+strMy= ['1','2','3','4']
+conc = myList.__add__(myList)
+
+print('concatanated list: ' + str(conc))
+print()
+
+sumOfList = sum(myList)
+print('sum of values in the list: '+ str(sumOfList))
+print()
+
+for index, element in enumerate(myList):
+    print(index, element)
+print()
+
+strList= '-'.join(str(myList))
+print(strList)
+print(strList[7])
+stringList = '-'.join(strMy)
+print(stringList)
+print("part of testU")
